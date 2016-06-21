d94473269bed8e803b4e0d97d9e370825e920601
Tomosuke Uemura <ftomo07c@gmail.com>
2016-06-21 09:56:37 +0000

    challenge2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -40,8 +40,8 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:name, :body)
-  end
+    params.require(:message).permit(:name, :body, :age)
+  end 
 
   def set_message
     @message = Message.find(params[:id])
